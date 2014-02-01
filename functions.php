e2eada891d9cfc6e6eb7765cfc9aa3c90ae6b531
Sal Ferrarello <salcode@salogic.net>
2014-02-01 14:21:54 -0500

    renamed includes directory to lib

--- functions.php
+++ functions.php
@@ -4,15 +4,12 @@
  *
  */
 
-
 // Add HTML5 markup structure
 add_theme_support( 'html5' );
-
-
 
 /**
  * Include all php files in the /includes directory
  *
  * https://gist.github.com/theandystratton/5924570
  */
-foreach ( glob( dirname( __FILE__ ) . '/includes/*.php' ) as $file ) { include $file; }
+foreach ( glob( dirname( __FILE__ ) . '/lib/*.php' ) as $file ) { include $file; }
