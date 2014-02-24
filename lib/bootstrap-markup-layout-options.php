3a072f2a026494c857523b825e1e08e8188360a0
Sal Ferrarello <salcode@salogic.net>
2014-02-23 22:13:03 -0500

    remove unused layouts from genesis options

--- lib/bootstrap-markup-layout-options.php
+++ lib/bootstrap-markup-layout-options.php
@@ -3,7 +3,18 @@
  * based on the Genesis template chosen
  */
 
+// remove unused layouts
+add_action('genesis_setup', 'bsg_remove_unused_layout_options', 15);
+
+// modify bootstrap classes based on genesis_site_layout
 add_filter('bsg-classes-to-add', 'bsg_modify_classes_based_on_template', 10, 3);
+
+function bsg_remove_unused_layout_options() {
+    genesis_unregister_layout( 'sidebar-content' );
+    genesis_unregister_layout( 'content-sidebar-sidebar' );
+    genesis_unregister_layout( 'sidebar-sidebar-content' );
+    genesis_unregister_layout( 'sidebar-content-sidebar' );
+}
 
 function bsg_layout_options_modify_classes_to_add( $classes_to_add ) {
 
