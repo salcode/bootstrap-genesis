e4ce9c9a072202d6c55ebcfc8cd6b52a633e3712
Sal Ferrarello <salcode@salogic.net>
2014-02-23 20:44:12 -0500

    remove full width image across the top of a single page (above content and sidebar(s)), instead always use featured-image ONLY above the content

--- lib/featured-image-display.php
+++ lib/featured-image-display.php
@@ -4,41 +4,29 @@ add_action('genesis_setup','bsg_featured_image_display', 15);
 
 function bsg_featured_image_display() {
     // image with post (disabled on single post / single page )
-    add_action( 'genesis_before_entry', 'srf_bootstrap_genesis_featured_image');
-
-    add_action( 'genesis_before_content', 'srf_bootstrap_genesis_featured_image_above_content' );
+    add_action( 'genesis_entry_header', 'srf_bootstrap_genesis_featured_image', 0);
 }
 
-// display featured image within the main container (next to sidebar)
-// on non-singular pages (potentially multiple featured images on these pages)
 function srf_bootstrap_genesis_featured_image() {
     global $post;
 
-    if ( is_singular() || !has_post_thumbnail()) {
-        // abort if singular or no thumbnail
+    if ( !has_post_thumbnail() ) {
         return;
     }
-    $attr = array(
-        'class' => 'archive-featured-image'
-    );
 
-    $permalink = get_permalink();
+    $featured_image_attr = array();
+
+    if ( is_singular() ) {
+        $featured_image_attr['class'] = 'single-featured-image';
+        // image does NOT link on single page
+        // (link would link back to the page we are already on)
+        $permalink = false;
+    } else {
+        $featured_image_attr['class'] = 'archive-featured-image';
+        $permalink = get_permalink();
+    }
 
     echo ( $permalink ? "<a href=\"{$permalink}\">" : "" );
-        the_post_thumbnail( 'blog-featured-image', $attr );
+        the_post_thumbnail( 'bsg-featured-image', $featured_image_attr );
     echo ( $permalink ? "</a>" : "" );
-}
-
-
-// display featured image above the main container and sidebar
-// on singular pages (only one featured image on these pages)
-function srf_bootstrap_genesis_featured_image_above_content() {
-    if ( !is_singular() || !has_post_thumbnail()) {
-        // abort if NOT singular or no thumbnail
-        return;
-    }
-    $attr = array(
-        'class' => 'single-featured-image span12'
-    );
-    the_post_thumbnail( 'full-width-featured-image', $attr );
 }
