c3bd8469da3909113fc2905ddc710ff0ef668a62
Sal Ferrarello <salcode@salogic.net>
2014-02-27 22:55:47 -0500

    update footer credentials to link to ironcodestudio

--- lib/footer-creds.php
+++ lib/footer-creds.php
@@ -3,6 +3,6 @@
 add_filter('genesis_footer_creds_text', 'bsg_footer_creds_filter');
 
 function bsg_footer_creds_filter( $creds ) {
-    $creds = '<a title="Bootstrap Genesis Starter Theme" href="https://github.com/salcode/bootstrap-genesis">Bootstrap Genesis Starter Theme</a> for the <a href="http://www.studiopress.com/themes/genesis" title="Genesis Framework">Genesis Framework</a> by <a title="Sal Ferrarello" href="http://salferrarello.com/">Sal Ferrarello</a>';
+    $creds = '<a title="Bootstrap Genesis Theme" href="http://ironcodestudio.com/bootstrap-genesis-theme/">Bootstrap Genesis Theme</a> by <a href="http://ironcodestudio.com/">Iron Code Studio</a>';
     return $creds;
 }
