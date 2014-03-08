54411aeb62da3bb2def3ffae6ba80817fad57275
Sal Ferrarello <salcode@salogic.net>
2014-03-08 16:14:28 -0500

    bootstrap nav on both primary and secondary nav locations

--- lib/nav.php
+++ lib/nav.php
@@ -5,10 +5,12 @@ add_action( 'genesis_setup', 'bsg_primary_nav_modifications', 15 );
 
 
 function bsg_primary_nav_modifications() {
-    // remove primary nav from default position
+    // remove primary & secondary nav from default position
     remove_action( 'genesis_after_header', 'genesis_do_nav' );
-    // add primary nav to top of the page
+    remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+    // add primary & secondary nav to top of the page
     add_action( 'genesis_before', 'genesis_do_nav' );
+    add_action( 'genesis_before', 'genesis_do_subnav' );
 
     // filter menu args for bootstrap walker and other settings
     add_filter( 'wp_nav_menu_args', 'bsg_nav_menu_args_filter' );
@@ -18,9 +20,11 @@ function bsg_primary_nav_modifications() {
 }
 
 function bsg_nav_menu_args_filter( $args ) {
-    error_log( '$args=' . print_r( $args, true ) );
 
-    if ( 'primary' === $args['theme_location'] ) {
+    if (
+        'primary' === $args['theme_location'] ||
+        'secondary' === $args['theme_location']
+    ) {
         $args['depth'] = 2;
         $args['menu_class'] = 'nav';
         $args['walker'] = new wp_bootstrap_navwalker();
@@ -30,20 +34,26 @@ function bsg_nav_menu_args_filter( $args ) {
 }
 
 function bsg_nav_menu_markup_filter( $html, $args ) {
-    return '<div class="navbar">' .
+
+    $output = '<div class="navbar">' .
         '<div class="navbar-inner">' .
-            '<div class="container-fluid">' .
-                '<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">' .
+            '<div class="container-fluid">';
+                $output .= '<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target="';
+                    $output .= '.nav-collapse' . sanitize_html_class( '-' . $args->theme_location );
+                    $output .= '">' .
                     '<span class="icon-bar"></span>' .
                     '<span class="icon-bar"></span>' .
                     '<span class="icon-bar"></span>' .
-                '</button>' .
+                '</button>';
                 //'<a class="brand" id="logo" title="Programming, parenting, and an abundance of other topics" href="http://salferrarello.com">Sal Ferrarello</a>' .
-                '<div class="nav-collapse collapse">' .
-                    $html .
+                $output .= '<div class="nav-collapse collapse ';
+                    $output .= 'nav-collapse' . sanitize_html_class( '-' . $args->theme_location );
+                $output .= '">';
+                    $output .= $html .
                 '</div>' .
             '</div>' .
         '</div>' .
     '</div>';
-}
 
+    return $output;
+}
