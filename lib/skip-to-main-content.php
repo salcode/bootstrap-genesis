76a139a61f89aba87fa8cb209b7172b0bd5bdfa3
Sal Ferrarello <salcode@salogic.net>
2014-03-21 16:01:50 -0400

    add markup and html id for skip-to-main-content link

---
+++ lib/skip-to-main-content.php
@@ -0,0 +1,20 @@
+<?php
+/* Skip to Main Content - Accessibility Link
+ *
+ */
+
+// add markup for the link
+add_action( 'genesis_before', 'bsg_skip_navigation_add_link' );
+// add id="main-content-container" as target
+add_filter( 'genesis_attr_content', 'bsg_skip_navigation_add_id_for_target' );
+
+
+function bsg_skip_navigation_add_link() {
+    echo '<a class="skip-to-main-content btn btn-large btn-danger" href="#main-content-container">skip to main content</a>';
+}
+
+function bsg_skip_navigation_add_id_for_target( $attr ) {
+    $attr['id'] = __( 'main-content-container', 'bsg' );
+
+    return $attr;
+}
