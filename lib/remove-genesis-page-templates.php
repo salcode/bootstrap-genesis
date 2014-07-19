f27dfe477eef43edb244af2b78e7d80e3b44ec8c
Sal Ferrarello <salcode@salogic.net>
2014-07-18 22:26:09 -0400

    Remove Genesis Templates archive and blog
    Code from Bill Erickson's article
    http://www.billerickson.net/remove-genesis-page-templates/
    
    Fixes #12

---
+++ lib/remove-genesis-page-templates.php
@@ -0,0 +1,15 @@
+<?php
+/* Remove Genesis Page Templates
+ *
+ * @author Bill Erickson
+ * @link http://www.billerickson.net/remove-genesis-page-templates
+ *
+ * @param array $page_templates
+ * @return array
+ */
+function bsg_be_remove_genesis_page_templates( $page_templates ) {
+	unset( $page_templates['page_archive.php'] );
+	unset( $page_templates['page_blog.php'] );
+	return $page_templates;
+}
+add_filter( 'theme_page_templates', 'bsg_be_remove_genesis_page_templates' );
