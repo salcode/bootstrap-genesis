7396b7da8960be2aac0442998943b031564f6c70
Sal Ferrarello <salcode@salogic.net>
2014-11-21 15:24:31 -0500

    remove sourcemap param on Grunt sass config
    sourcemap now defaults to true to grunt-contrib-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,12 +96,12 @@ module.exports = function(grunt) {
         sass: {
             // `grunt sass:dev`
             dev: {
-                options: { style: "normal", sourcemap: true },
+                options: { style: "normal" },
                 files: sass_files,
             },
             // `grunt sass:prod`
             prod: {
-                options: { style: "compressed", sourcemap: true },
+                options: { style: "compressed" },
                 files: sass_files,
             }
 
