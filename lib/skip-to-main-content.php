eeeca9e3f3566e23ac4a543b070465b06d1e43e8
Sal Ferrarello <sal@launchdm.com>
2015-01-30 15:04:40 -0500

    move "skip to main content" link above nav
    Reduce the priority value of the function that displays the "skip to
    main content" link.  This places it above the nav
    
    Fixes #26

--- lib/skip-to-main-content.php
+++ lib/skip-to-main-content.php
@@ -4,7 +4,7 @@
  */
 
 // add markup for the link
-add_action( 'genesis_before', 'bsg_skip_navigation_add_link' );
+add_action( 'genesis_before', 'bsg_skip_navigation_add_link', 5 );
 // add id="main-content-container" as target
 add_filter( 'genesis_attr_content', 'bsg_skip_navigation_add_id_for_target' );
 
