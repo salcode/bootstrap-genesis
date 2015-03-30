df0811a839bb6f0b1ea1e5ce78f2f946e65ca5da
Sal Ferrarello <salcode@salogic.net>
2015-03-30 14:28:20 -0400

    Update meta viewport to remove maximum-scale
    We're now using the default Genesis mobile viewport
    
    Fixes #74

--- lib/responsive-viewport.php
+++ lib/responsive-viewport.php
@@ -1,15 +1,3 @@
 <?php
-
 // Add viewport meta tag for mobile browsers
-// genesis-responsive-viewport does NOT include maximum-scale=1
-// and does NOT have a filter to modify, therefore we're using our own code
-//add_theme_support( 'genesis-responsive-viewport' );
-add_action( 'genesis_meta', 'bsg_responsive_viewport');
-
-// add viewport for responsive
-// http://stackoverflow.com/questions/9386429/simple-bootstrap-page-is-not-responsive-on-the-iphone#answer-10120831
-function bsg_responsive_viewport() {
-    ?>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<?php
-}
+add_theme_support( 'genesis-responsive-viewport' );
