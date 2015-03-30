c8fe2ad08ced33b614640793e2cc64b9523cd665
Sal Ferrarello <salcode@salogic.net>
2015-03-30 14:35:10 -0400

    On all but is_front_page() nofollow footer links
    Fixes #71

--- lib/footer-creds.php
+++ lib/footer-creds.php
@@ -3,6 +3,7 @@
 add_filter('genesis_footer_creds_text', 'bsg_footer_creds_filter');
 
 function bsg_footer_creds_filter( $creds ) {
-    $creds = '<a title="Bootstrap Genesis Theme" href="http://ironcodestudio.com/bootstrap-genesis-theme/">Bootstrap Genesis Theme</a> by <a href="http://ironcodestudio.com/">Iron Code Studio</a>';
+    $rel = is_front_page() ? '' : 'rel="nofollow"';
+    $creds = "<a {$rel} href=\"http://ironcodestudio.com/bootstrap-genesis-theme/\">Bootstrap Genesis Theme</a> by <a {$rel} href=\"http://ironcodestudio.com/\">Iron Code Studio</a>";
     return $creds;
 }
