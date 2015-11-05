b63cf1673ad246ed21e6fc3edb74098d2087709f
Eli Dalbey <eli@launchdm.com>
2015-11-05 15:13:47 -0500

    Indent head/meta tags. Fixes #114

--- lib/ie-conditional-comments.php
+++ lib/ie-conditional-comments.php
@@ -7,17 +7,17 @@ add_action(     'genesis_doctype', 'bsg_conditional_comments' );
 
 function bsg_conditional_comments() {
    ?>
-<!doctype html>
-<!--[if lt IE 7 ]> <html class="ie ie6 no-js" dir="ltr" lang="en-US"> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie ie7 no-js" dir="ltr" lang="en-US"> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie ie8 no-js" dir="ltr" lang="en-US"> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie ie9 no-js" dir="ltr" lang="en-US"> <![endif]-->
-<!--[if gt IE 9]><!--><html class="no-js" dir="ltr" lang="en-US"><!--<![endif]-->
-<?php /*
- // add IE conditional comments to html element
- // http://www.paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
- */ ?>
-<head profile="http://gmpg.org/xfn/11">
-<meta charset="UTF-8" />
+	<!doctype html>
+	<!--[if lt IE 7 ]> <html class="ie ie6 no-js" dir="ltr" lang="en-US"> <![endif]-->
+	<!--[if IE 7 ]>    <html class="ie ie7 no-js" dir="ltr" lang="en-US"> <![endif]-->
+	<!--[if IE 8 ]>    <html class="ie ie8 no-js" dir="ltr" lang="en-US"> <![endif]-->
+	<!--[if IE 9 ]>    <html class="ie ie9 no-js" dir="ltr" lang="en-US"> <![endif]-->
+	<!--[if gt IE 9]><!--><html class="no-js" dir="ltr" lang="en-US"><!--<![endif]-->
+	<?php /*
+	 // add IE conditional comments to html element
+	 // http://www.paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
+	 */ ?>
+	<head profile="http://gmpg.org/xfn/11">
+	<meta charset="UTF-8" />
    <?php
 }
