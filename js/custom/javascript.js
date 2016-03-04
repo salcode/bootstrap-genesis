6bed01dc9677b799f5759b120e80ef7929843cc3
Bryan Willis <willis.bryan@live.com>
2016-03-03 20:24:10 -0600

    Media query compatibility Internet Explorer 10 in Windows 8 and Windows Phone 8
    Currently we're not supporting IE10 and windows phone 8 media queries

--- js/custom/javascript.js
+++ js/custom/javascript.js
@@ -1,3 +1,18 @@
 jQuery(document).ready(function($) {
     $('html').removeClass('no-js');
 });
+
+// Copyright 2014-2015 Twitter, Inc.
+// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+(function () {
+  'use strict';
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style');
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    );
+    document.querySelector('head').appendChild(msViewportStyle);
+  }
+})();
